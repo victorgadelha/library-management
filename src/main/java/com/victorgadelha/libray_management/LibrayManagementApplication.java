70c7249dbe5afb0a80f8ae3e0c8797be7ba88b0f
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-07-22 12:42:27 -0300

    first commit

---
+++ src/main/java/com/victorgadelha/libray_management/LibrayManagementApplication.java
@@ -0,0 +1,13 @@
+package com.victorgadelha.libray_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrayManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrayManagementApplication.class, args);
+	}
+
+}
