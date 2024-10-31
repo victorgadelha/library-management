3aaea09768e87c58968c3cf1e9468b067e618b6f
Victor <joaovictor_gadelha@live.com>
2024-10-31 14:13:56 -0300

    cleanup: removendo imports desnecessários

--- src/main/java/com/victorgadelha/libray_management/LibrayManagementApplication.java
+++ src/main/java/com/victorgadelha/libray_management/LibrayManagementApplication.java
@@ -1,10 +1,7 @@
 package com.victorgadelha.libray_management;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.victorgadelha.libray_management.faker.FakeData;
 
 @SpringBootApplication
 public class LibrayManagementApplication {
