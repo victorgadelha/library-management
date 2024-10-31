03421c1ef4703b24f562755cd2174941ee75577c
Victor <joaovictor_gadelha@live.com>
2024-10-31 10:53:05 -0300

    feat: implementando a exclusão de livros por ID

--- src/main/java/com/victorgadelha/libray_management/controllers/BookController.java
+++ src/main/java/com/victorgadelha/libray_management/controllers/BookController.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,6 +69,13 @@ public class BookController {
                 updatedBook.getLanguages(), updatedBook.getUpdatedAt());
 
         return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
+    }
+
+    @DeleteMapping("/books/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable UUID id) {
+        this.bookService.deleteBookByID(id);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 }
