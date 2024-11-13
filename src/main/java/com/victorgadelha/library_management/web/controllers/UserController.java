e4e192fb83606bd1a62f53fb9df7aa58d6371699
Victor <joaovictor_gadelha@live.com>
2024-11-13 10:36:03 -0300

    feat: implementando cadastro de usuario para testar o controller

--- src/main/java/com/victorgadelha/library_management/web/controllers/UserController.java
+++ src/main/java/com/victorgadelha/library_management/web/controllers/UserController.java
@@ -1,16 +1,26 @@
-// package com.victorgadelha.library_management.web.controllers;
+package com.victorgadelha.library_management.web.controllers;
 
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// @RestController
-// @RequestMapping("/users")
-// public class UserController {
-    
-//     @GetMapping
-//     public ResponseEntity<?> findAll() {
+import com.victorgadelha.library_management.domain.entities.User;
+import com.victorgadelha.library_management.infra.adapters.gateway.repositories.user.JpaUserRepository;
 
-//     }
-// }
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    JpaUserRepository jpaUserRepository;
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        this.jpaUserRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+}
