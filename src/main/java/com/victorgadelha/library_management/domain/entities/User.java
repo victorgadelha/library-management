c941b7708f6e9564ec787da1cad7e36e6dec0a49
Victor <joaovictor_gadelha@live.com>
2024-11-19 16:40:54 -0300

    refactor: deletando arquivo e import nao utilizado

--- src/main/java/com/victorgadelha/library_management/domain/entities/User.java
+++ src/main/java/com/victorgadelha/library_management/domain/entities/User.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-import com.victorgadelha.library_management.domain.enums.RoleValue;
 import com.victorgadelha.library_management.domain.enums.UserType;
 
 import jakarta.persistence.*;
--- src/main/java/com/victorgadelha/library_management/web/dtos/CreateUserResponseDTO.java
+++
@@ -1,9 +0,0 @@
-package com.victorgadelha.library_management.web.dtos;
-
-import java.util.UUID;
-
-import com.victorgadelha.library_management.domain.enums.UserType;
-
-public record CreateUserResponseDTO(UUID id, String name, String email, UserType role) {
-    
-}
