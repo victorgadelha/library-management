d0491f206a507cd2c76174494cedea4c1ac9da2b
Victor <joaovictor_gadelha@live.com>
2024-11-21 14:20:27 -0300

    feat: adicionando caso de uso para achar usuário pelo email

---
+++ src/main/java/com/victorgadelha/library_management/app/usecases/user/FindUserByEmailUseCase.java
@@ -0,0 +1,27 @@
+package com.victorgadelha.library_management.app.usecases.user;
+
+import org.springframework.stereotype.Service;
+
+import com.victorgadelha.library_management.domain.repositories.UserRepository;
+import com.victorgadelha.library_management.infra.mappers.UserMapper;
+import com.victorgadelha.library_management.web.dtos.user.UserDTO;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class FindUserByEmailUseCase {
+    public FindUserByEmailUseCase(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserDTO execute(String email) {
+        var user = this.userRepository.findByEmail(email)
+                .orElseThrow(() -> new EntityNotFoundException("User not found."));
+
+        return this.userMapper.toDTO(user);
+    }
+}
