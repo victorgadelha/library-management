77790268af0c1530cbd6cf716c1a6b27dbc6a71a
Victor <joaovictor_gadelha@live.com>
2024-11-21 14:15:55 -0300

    refactor: separando métodos de login e cadastro

--- src/main/java/com/victorgadelha/library_management/web/controllers/UserController.java
+++ src/main/java/com/victorgadelha/library_management/web/controllers/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.victorgadelha.library_management.app.usecases.user.CreateUserUseCase;
-import com.victorgadelha.library_management.infra.mappers.UserMapper;
 import com.victorgadelha.library_management.web.dtos.user.CreateUserRequestDTO;
 import com.victorgadelha.library_management.web.dtos.user.CreateUserResponseDTO;
 
@@ -18,19 +17,13 @@ public class UserController {
 
     private final CreateUserUseCase createUserUseCase;
 
-    public UserController(UserMapper userMapper, CreateUserUseCase createUserUseCase) {
+
+    public UserController(CreateUserUseCase createUserUseCase) {
         this.createUserUseCase = createUserUseCase;
     }
 
     @PostMapping("/sign-up")
     public ResponseEntity<CreateUserResponseDTO> createUser(@RequestBody CreateUserRequestDTO userDTO) {
-
-        var user = this.createUserUseCase.execute(userDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(user);
-    }
-
-    @PostMapping("/sign-in")
-    public ResponseEntity<CreateUserResponseDTO> login(@RequestBody CreateUserRequestDTO userDTO) {
 
         var user = this.createUserUseCase.execute(userDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
