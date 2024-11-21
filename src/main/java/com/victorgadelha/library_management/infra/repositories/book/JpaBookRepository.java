e071738ed33f9e21b0d959b3e777c0cf98100957
Victor <joaovictor_gadelha@live.com>
2024-11-21 14:23:00 -0300

    refactor: gerando novamente os repositórios de book

---
+++ src/main/java/com/victorgadelha/library_management/infra/repositories/book/JpaBookRepository.java
@@ -0,0 +1,13 @@
+package com.victorgadelha.library_management.infra.repositories.book;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.victorgadelha.library_management.domain.entities.Book;
+
+@Repository
+public interface JpaBookRepository extends JpaRepository<Book, UUID> {
+
+}
