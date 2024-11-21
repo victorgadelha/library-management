5a2e93c210d4df8a45bbacf1adb414670b185bd6
Victor <joaovictor_gadelha@live.com>
2024-11-21 14:17:10 -0300

    refacor: alterando DTO de usuario para incluir senha e role

--- src/main/java/com/victorgadelha/library_management/web/dtos/user/UserDTO.java
+++ src/main/java/com/victorgadelha/library_management/web/dtos/user/UserDTO.java
@@ -3,13 +3,14 @@ package com.victorgadelha.library_management.web.dtos.user;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import com.victorgadelha.library_management.domain.enums.UserType;
+import com.victorgadelha.library_management.domain.enums.Role;
 
 public record UserDTO(
-        UUID id,
-        String name,
-        String email,
-        UserType type,
-        LocalDateTime createdAt,
-        LocalDateTime updatedAt) {
+                UUID id,
+                String name,
+                String email,
+                String password,
+                Role role,
+                LocalDateTime createdAt,
+                LocalDateTime updatedAt) {
 }
