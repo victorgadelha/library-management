5009241677ac2ccd98b2d6c69d718ba7c8516050
Victor <joaovictor_gadelha@live.com>
2024-11-21 14:22:25 -0300

    refactor: gerando novamente os faker de books

---
+++ src/main/java/com/victorgadelha/library_management/infra/faker/FakeData.java
@@ -0,0 +1,24 @@
+package com.victorgadelha.library_management.infra.faker;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+import com.victorgadelha.library_management.infra.repositories.book.JpaBookRepository;
+
+@Service
+public class FakeData implements CommandLineRunner {
+
+    private final JpaBookRepository bookRepository;
+    private final FakeDataGenerator fakeDataGenerator;
+    
+    public FakeData(JpaBookRepository bookRepository, FakeDataGeneratorImpl fakeDataGenerator) {
+        this.bookRepository = bookRepository;
+        this.fakeDataGenerator = fakeDataGenerator;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        var books = this.fakeDataGenerator.generateFakeBooks(100);
+        this.bookRepository.saveAll(books);
+    }
+}
