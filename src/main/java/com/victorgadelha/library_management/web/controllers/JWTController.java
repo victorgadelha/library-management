8664045c6780abce26db2597cc38986fd3fd21d6
Victor <joaovictor_gadelha@live.com>
2024-11-21 14:14:20 -0300

    feat: implementando login no controller com jwt

---
+++ src/main/java/com/victorgadelha/library_management/web/controllers/JWTController.java
@@ -0,0 +1,27 @@
+package com.victorgadelha.library_management.web.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.victorgadelha.library_management.app.usecases.user.LoginUseCase;
+import com.victorgadelha.library_management.web.dtos.jwt.JWTResponseDTO;
+import com.victorgadelha.library_management.web.dtos.user.CreateUserRequestDTO;
+
+@RestController
+public class JWTController {
+    private final LoginUseCase loginUseCase;
+
+    public JWTController(LoginUseCase loginUseCase) {
+        this.loginUseCase = loginUseCase;
+    }
+
+    @PostMapping("users/sign-in")
+    public ResponseEntity<JWTResponseDTO> login(@RequestBody CreateUserRequestDTO userDTO) {
+
+        var JWT = this.loginUseCase.execute(userDTO.email(), userDTO.password());
+        return ResponseEntity.status(HttpStatus.OK).body(JWT);
+    }
+}
