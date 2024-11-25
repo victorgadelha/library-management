dddc18b8393e3edb5120877fd50e7551d6648cd4
Victor <joaovictor_gadelha@live.com>
2024-11-25 13:29:03 -0300

    feat: adicionando novos mapeamentos de DTO

--- src/main/java/com/victorgadelha/library_management/infra/mappers/UserMapper.java
+++ src/main/java/com/victorgadelha/library_management/infra/mappers/UserMapper.java
@@ -3,18 +3,23 @@ package com.victorgadelha.library_management.infra.mappers;
 import org.mapstruct.Mapper;
 
 import com.victorgadelha.library_management.domain.entities.User;
+import com.victorgadelha.library_management.web.dtos.user.AuthUserRequestDTO;
 import com.victorgadelha.library_management.web.dtos.user.CreateUserRequestDTO;
 import com.victorgadelha.library_management.web.dtos.user.CreateUserResponseDTO;
 import com.victorgadelha.library_management.web.dtos.user.UserDTO;
+import com.victorgadelha.library_management.web.dtos.user.UserProfileDTO;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
     UserDTO toDTO(User user);
 
-    // // Mapeamento de DTO para entidade
+    UserProfileDTO toUserProfileDTO(User user);
+
     User toEntity(CreateUserRequestDTO createUserRequestDTO);
 
     CreateUserResponseDTO toCreateUserResponseDTO(User user);
+
+    AuthUserRequestDTO toAuthUserRequestDTO(User user);
 
 }
--- src/main/java/com/victorgadelha/library_management/web/dtos/user/UserProfileDTO.java
+++ src/main/java/com/victorgadelha/library_management/web/dtos/user/UserProfileDTO.java
@@ -1,15 +1,20 @@
 package com.victorgadelha.library_management.web.dtos.user;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
+import com.victorgadelha.library_management.domain.entities.Loan;
+import com.victorgadelha.library_management.domain.entities.Reservation;
 import com.victorgadelha.library_management.domain.enums.Role;
 
-public record UserDTO(
+public record UserProfileDTO(
         UUID id,
         String name,
         String email,
         Role role,
+        List<Loan> loans,
+        List<Reservation> reservations,
         LocalDateTime createdAt,
         LocalDateTime updatedAt) {
 }
