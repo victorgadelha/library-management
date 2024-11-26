6baf057ed69e5241aad783b2ea8c7785b928714f
Victor <joaovictor_gadelha@live.com>
2024-11-26 16:46:53 -0300

    style: ajustando identação

--- src/main/java/com/victorgadelha/library_management/web/dtos/book/BookDTO.java
+++ src/main/java/com/victorgadelha/library_management/web/dtos/book/BookDTO.java
@@ -3,6 +3,12 @@ package com.victorgadelha.library_management.web.dtos.book;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record BookDTO(UUID id, String isbn, String title, String author, String languages, LocalDateTime createdAt,
-        LocalDateTime updatedAt) {
+public record BookDTO(
+                UUID id,
+                String isbn,
+                String title,
+                String author,
+                String languages,
+                LocalDateTime createdAt,
+                LocalDateTime updatedAt) {
 }
